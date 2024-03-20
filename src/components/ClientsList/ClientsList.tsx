fe672b8386668ef8088e0a770b77cbf188fc2a36
Tarso Brietzke Iracet <tarsobrietzkeiracet@gmail.com>
2024-03-20 19:17:45 -0300

    Version 1.6

--- src/components/ClientsList/ClientsList.tsx
+++ src/components/ClientsList/ClientsList.tsx
@@ -1,12 +1,16 @@
 import styles from "./ClientsList.module.css";
 
 import { useFetchData } from "../../hooks/useFetchData";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Loading from "../Loading/Loading";
+import { Link } from "react-router-dom";
+import useAuthenticate from "../../hooks/useAuthenticate";
 
 const ClientsList = () => {
   const url: string = "/clients";
   const { handleFetch, data: users, loading, error } = useFetchData(url);
+
+  const { user: userContext } = useAuthenticate();
 
   useEffect(() => {
     handleFetch();
@@ -22,30 +26,62 @@ const ClientsList = () => {
             <>
               {users.length > 0 ? (
                 users.map((user) => (
-                  <div key={user._id} className={styles.clients}>
-                    <div className={styles.clients_left}>
-                      <h2>Nome: {user.firstName}</h2>
-                      <p>
-                        {user.address.city} - {user.address.state}
-                      </p>
-                    </div>
-                    <div className={styles.clients_rigth}>
-                      <div className={styles.status}>
-                        <p
-                          style={{
-                            color: user.status
-                              ? "rgb(45, 220, 15)"
-                              : "rgb(167, 0, 0)",
-                          }}
-                        >
-                          <span>Status:</span>
-                          {user.status
-                            ? " Vistoria realizada!"
-                            : " Vistoria não realizada!"}
-                        </p>
+                  <div key={user._id}>
+                    {userContext && userContext.surveyor === true ? (
+                      <Link className={styles.link} to={`/Vistory/${user._id}`}>
+                        <div className={styles.clients}>
+                          <div className={styles.clients_left}>
+                            <h2>Nome: {user.firstName}</h2>
+                            <p>
+                              {user.address.city} - {user.address.state}
+                            </p>
+                          </div>
+                          <div className={styles.clients_rigth}>
+                            <div className={styles.status}>
+                              <p
+                                style={{
+                                  color: user.status
+                                    ? "rgb(45, 220, 15)"
+                                    : "rgb(167, 0, 0)",
+                                }}
+                              >
+                                <span>Status:</span>
+                                {user.status
+                                  ? " Vistoria realizada!"
+                                  : " Vistoria não realizada!"}
+                              </p>
+                            </div>
+                            <p>Publicado: {user.created_at}</p>
+                          </div>
+                        </div>
+                      </Link>
+                    ) : (
+                      <div key={user._id} className={styles.clients}>
+                        <div className={styles.clients_left}>
+                          <h2>Nome: {user.firstName}</h2>
+                          <p>
+                            {user.address.city} - {user.address.state}
+                          </p>
+                        </div>
+                        <div className={styles.clients_rigth}>
+                          <div className={styles.status}>
+                            <p
+                              style={{
+                                color: user.status
+                                  ? "rgb(45, 220, 15)"
+                                  : "rgb(167, 0, 0)",
+                              }}
+                            >
+                              <span>Status:</span>
+                              {user.status
+                                ? " Vistoria realizada!"
+                                : " Vistoria não realizada!"}
+                            </p>
+                          </div>
+                          <p>Publicado: {user.created_at}</p>
+                        </div>
                       </div>
-                      <p>Publicado: {user.created_at}</p>
-                    </div>
+                    )}
                   </div>
                 ))
               ) : (
@@ -53,7 +89,7 @@ const ClientsList = () => {
               )}
             </>
           ) : (
-            <div className={styles.loading}>
+            <div className="loading">
               <Loading />
             </div>
           )}
